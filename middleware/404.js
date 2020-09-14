b36439d39f1e32878cd486e363abc5c00e1079ee
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-09-14 23:37:05 +0300

    not finish

---
+++ middleware/404.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+module.exports =  (req, res ) => {
+    res.status(404);
+    res.StatusMessage('404 Not Found');
+    res.json({error : 'Page Not Found !!'});
+}
+
+
+
+
---
+++ middleware/500.js
@@ -0,0 +1,9 @@
+
+'use strict'
+
+module.exports = (req, res) => {
+    console.log("BE Atenttion ERROR !!!")
+    res.status(500);
+    res.StatuesMessage = 'Error Server!!';
+    res.json({ error: err });
+}
